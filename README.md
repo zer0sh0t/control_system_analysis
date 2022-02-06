097facf41d922ce42e31b3b17d5cb07103b759a0
kumar-harin <haryn.k10@gmail.com>
2022-02-06 19:48:57 +0530

    updating example

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-`control.py` contains simple implementations of bode diagram and root locus methods, which are used to analyse the frequency response and stability of a control system.
+`control.py` contains simple implementations of bode diagram and root locus methods, which are used to analyse the frequency response and stability of a control system, along with a handy pid controller.
 
 #### bode diagram
 ![plot_0](images/plot_0.png)
--- example.py
+++ example.py
@@ -1,18 +1,27 @@
 import control as ctrl
 
+def analyse(F):
+    # to plot zeros and poles on the s-plane
+    ctrl.roots(F) 
+    
+    # to plot bode diagram
+    ctrl.bode(F) # specify desired range as a 2nd argument
+
+    # to plot root locus
+    ctrl.rlocus(F) # specify desired range as a 2nd argument
+
+    # to plot the results on just a single gain value
+    ctrl.rlocus(F, [2])
+
 # for a tranfer function F = (s**3 + 2 * s**2 + 3 * s + 4) / (s**3 + 4 * s**2 + 9 * s + 20)
 num = [1, 2, 3, 4] # coeffs of numerator
 den = [1, 4, 9, 20] # coeffs of denominator
 F = ctrl.tf(num, den)
+analyse(F)
 
-# to plot zeros and poles on the s-plane
-ctrl.roots(F) 
-
-# to plot bode diagram
-ctrl.bode(F) # specify desired range as a 2nd argument
-
-# to plot root locus
-ctrl.rlocus(F) # specify desired range as a 2nd argument
- 
-# to plot the results on just a single gain value
-ctrl.rlocus(F, [2])
+Kp = 1
+Ki = 0.15
+Kd = 0.67
+pid = ctrl.pid_controller(Kp, Ki, Kd)
+F = pid * F # adding pid controller to the system
+analyse(F)
